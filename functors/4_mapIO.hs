a410d015cf944020866f8525345a9ea88263d2a2
João Esperancinha <jofisaes@gmail.com>
2024-08-16 09:14:10 +0200

    sets alternative

--- functors/4_mapIO.hs
+++ functors/4_mapIO.hs
@@ -1,5 +1,7 @@
 main :: IO ()
 main = do
+--  Some GHC versions do not recognize :: IO Int
+--  let ioGet5Action = return 5
   let ioGet5Action :: IO Int = return 5
   let ioMappedResult = fmap (*2) ioGet5Action
   result <- ioMappedResult
