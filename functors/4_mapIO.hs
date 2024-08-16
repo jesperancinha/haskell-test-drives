8582429a559e030d152b728a7e1c7c06f304b092
João Esperancinha <jofisaes@gmail.com>
2024-08-16 09:16:40 +0200

    sets alternative

--- functors/4_mapIO.hs
+++ functors/4_mapIO.hs
@@ -3,6 +3,9 @@ main = do
 --  Some GHC versions do not recognize :: IO Int
 --  let ioGet5Action = return 5
   let ioGet5Action :: IO Int = return 5
+-- An IO is a computation that performs with side-effects
+-- When we invoke it by trying to print, it interprets as a computation and looks for
+-- Show IO, which doesn't exist for a computation of type IO
   let ioMappedResult = fmap (*2) ioGet5Action
   result <- ioMappedResult
   print result
