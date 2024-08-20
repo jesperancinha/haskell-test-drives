2b7f4fe5c509ead86eb9077be9ce1076937ecc04
jesperancinha <jofisaes@gmail.com>
2024-08-20 09:54:28 +0200

    clean all

--- Makefile.mk
+++ Makefile.mk
@@ -1,4 +1,4 @@
 clean-all:
-	ls -I '*.*' functors | xargs -I {} rm functors/{}
+	ls -I '*.*' functors | grep -v "Makefile" | xargs -I {} rm functors/{}
 	ls functors/*.o | xargs -I {} rm {}
 	ls functors/*.hi | xargs -I {} rm {}
