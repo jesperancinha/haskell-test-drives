9041e2de175e3eab05ce03a7bc72ab94fbd13e8e
João Esperancinha <jofisaes@gmail.com>
2024-10-07 23:50:23 +0200

    Identity test for haskell

--- functors/1_mapList.hs
+++ functors/1_mapList.hs
@@ -4,6 +4,9 @@ addOne x = x + 1
 square :: Int -> Int
 square x = x * x
 
+identityFunction :: Int -> Int
+identityFunction x = x
+
 main :: IO ()
 main = do
     print (fmap addOne (Just 5))
@@ -11,4 +14,5 @@ main = do
     print (fmap addOne [1, 2, 3, 4, 5])
     print (fmap square [1, 2, 3, 4, 5])
     print ([1,2,3,4,5])
-
+    print (fmap identityFunction [1,2,3,4,5])
+    print (fmap (addOne . square) [1,2,3,4,5] == (fmap addOne . fmap square) [1,2,3,4,5])
